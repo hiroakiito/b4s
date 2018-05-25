c66e86c360aa3ebaf1ab6bc3dcfb713c919ad4ac
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-05-25 07:31:47 +0000

    new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
   end
 end
