974213be581f42ab0796a33405863c4033dfbf1a
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-05-25 08:11:57 +0000

    gazou

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,21 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ようこそB4Sへ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525060525) do
+ActiveRecord::Schema.define(version: 20180525054551) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20180525060525) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
